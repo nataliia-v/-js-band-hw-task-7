130a33ea8c9a7424787864a86d39e795ac2df03b
nataliia-v <nataly.verbenskaya@gmail.com>
2019-12-04 13:52:02 +0200

    ZXC-2: + created store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/RootReduced';
 import App from './components/App/App';
 
 import './index.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
